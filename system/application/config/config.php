26f3b050e4fee97cd3b6cc9d8c6323bd2857e233
the_misiek <michal.jedruch@ebicom.pl>
2012-09-02 13:37:31 +0200

    zmiana pliku configu.

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://localhost:98/quadrigis/";
+$config['base_url']	= "http://localhost:98/qWebSite/";
 
 /*
 |--------------------------------------------------------------------------
